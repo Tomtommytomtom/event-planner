f82c89abf7e9704bd6f7fd0f485cd9483e332cff
Tom <tom.pillokat@googlemail.com>
2019-12-26 02:08:17 +0100

    Add new color theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes:{
+            light: {
+                primary: '#F07F1D',
+                success: '#00FA9A',
+                info: '#1E90FF',
+                error: 	'#FF1493',
+            }
+        }
+    }
 });
