1b359ee61935d2ed9f1fcd753fc1f0f67b0bd57d
Tom <tom.pillokat@googlemail.com>
2019-12-26 02:25:06 +0100

    Add empty components

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,26 @@
         <h1><span class="font-weight-light">EVENT</span><span class="font-weight-regular grey--text text--darken-4">PLANNER</span></h1>
     </v-app-bar>
     <v-content>
-      
+      <Calendar></Calendar>
+      <EventForm></EventForm>
+      <Notifications></Notifications>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Calendar from '@/components/Calendar'
+import EventForm from '@/components/EventForm'
+import Notifications from '@/components/Notifications'
 
 export default {
+  
   name: 'App',
 
   components: {
+    Calendar,
+    EventForm,
+    Notifications    
   },
 
   data: () => ({
