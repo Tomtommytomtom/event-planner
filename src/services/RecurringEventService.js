a69cdf7bae82544f68c4dacabdf40f7ee1019291
Tom <tom.pillokat@googlemail.com>
2020-01-09 23:16:03 +0100

    Remove most of the code in RecurringEventService
    -Remove functions that are now implemented in Events and subclasses

--- src/services/RecurringEventService.js
+++ src/services/RecurringEventService.js
@@ -1,8 +1,6 @@
 import EventService from './EventService'
 import DateArithmetic from './DateArithmetic'
 
-
-
 const applySingleRecurringToStatic = event => {    
     console.log(event)     
     let eventToAdd = EventService.createAddAndReturnRecurring(event)
@@ -21,74 +19,6 @@ const applySingleRecurringToStatic = event => {
 const shouldBeApplied = (eventStart, date) => {
     return DateArithmetic.doesEventStartBeforeDate(eventStart, date)
 }
-
-// const getYearsToAddInDays = (event) => {
-//     const dayInAYear = 365
-//     const type = event.type.split('-')[0]
-
-//     switch(type){
-//         case 'daily':
-//             return dayInAYear * event.frequenzy * 2
-//         case 'weekly':
-//             return dayInAYear * event.frequenzy * 7
-//         case 'monthly':
-//             return dayInAYear * 50
-//         case 'annually':
-//             return dayInAYear * 200
-//         case 'weekdays':
-//             return dayInAYear * 2
-//     }
-// } 
-
-
-
-const getNextDateForMonthly = event => {
-    return getNthWeekdayForMonthly(event)
-    
-}
-
-const getNthWeekdayForMonthly = (event) => {
-    const [nth,weekday] = DateArithmetic.getNthWeekday(event.start)
-    const nextMonth = getNextMonth(event.start)
-
-    const eventStart = DateArithmetic.getNthWeekdayOfMonth(nth,weekday, nextMonth)
-    const eventEnd = DateArithmetic.addDaysToDate(eventStart, getDurationOfEvent(event))
-    
-    return {
-        start: eventStart,
-        end: eventEnd
-    }
-}
-
-const getLastWeekdayForMonthly = (event) => {
-    const weekday = DateArithmetic.getWeekday(event.start)
-    const duration = getDurationOfEvent(event)
-    
-    const nextMonth = getNextMonth(event.start)
-
-    const start = DateArithmetic.getDateofLastWeekdayInMonth(weekday,nextMonth)
-    const end = DateArithmetic.addDaysToDate(start, duration)
-
-    return { start, end }
-}
-
-const getNextDateForWeekdays = event => {
-    return {
-        start: DateArithmetic.getNextWeekday(event.start),
-        end: DateArithmetic.getNextWeekday(event.end)
-    }
-}
-
-const getNextMonth = date => {
-    const [year, month, day] = date.split('-')
-
-    if(+month + 1 < 13){
-      return `${year}-${+month + 1}-01` 
-    } else {
-      return `${+year + 1}-01-01`
-    }
-  }
-
 
 export default {
     applySingleRecurringToStatic
