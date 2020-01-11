5d30ab2b4a0445be9401550b11723d4cafd26b98
Tom <tom.pillokat@googlemail.com>
2020-01-11 17:02:36 +0100

    Add tests for DateArithmetic module

--- test/unit/DateArithmetic.test.js
+++ test/unit/DateArithmetic.test.js
@@ -20,6 +20,38 @@ test('should return the date of last day of a month', () => {
     expect(getLastDayOfMonth('2020-12-14')).toBe('2020-12-31')
 })
 
+test('should convert YYYY-MM-DD string to dateobject', () => {
+    const dateStringToObject = DateArithmetic.dateStringToObject
+
+    let dateObject = new Date(2020,7,27)
+    expect(dateStringToObject('2020-08-27 19:00')).toMatchObject(dateObject)
+    dateObject = new Date(2020,0,1)
+    expect(dateStringToObject('2020-01-01')).toMatchObject(dateObject)
+    dateObject = new Date(2020,11,24)
+    expect(dateStringToObject('2020-12-24')).toMatchObject(dateObject)
+
+})
+
+test('should convert dateObject to YYYY-MM-DD string', () => {
+    const dateObjectToString = DateArithmetic.dateObjectToString
+
+    let dateObject = new Date(2019,2,5)
+    expect(dateObjectToString(dateObject)).toBe('2019-03-04')
+    dateObject = new Date(2030,1,28)
+    expect(dateObjectToString(dateObject)).toBe('2030-02-27')
+})
+
+test('should return true if YYYY-MM-DD date one is before date two',() =>{
+    const isDateBeforeDate = DateArithmetic.isDateBeforeDate
+
+    expect(isDateBeforeDate('2020-02-01','2020-05-13')).toBe(true)
+    expect(isDateBeforeDate('2020-07-01','2020-05-13')).toBe(false)
+    expect(isDateBeforeDate('2025-02-01','2020-01-01')).toBe(false)
+})
+
+
+
+
 
 
 
